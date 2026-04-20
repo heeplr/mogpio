6624846ea89d275bd76d1130ebcd420a0b3b0712
daniel <d@upquark>
2026-04-20 19:17:21 +0200

    use color prompt in terminal (hihi)

--- src/microrl_user_config.h
+++ src/microrl_user_config.h
@@ -6,7 +6,7 @@ extern "C" {
 #endif
 
 #define MICRORL_CFG_USE_COMPLETE        1
-#define MICRORL_CFG_USE_PROMPT_COLOR    0
+#define MICRORL_CFG_USE_PROMPT_COLOR    1
 #define MICRORL_CFG_PROMPT_ON_INIT      0
 #define MICRORL_CFG_PROMPT_STRING       "mogpio> "
 #define MICRORL_CFG_CMDLINE_LEN         80
